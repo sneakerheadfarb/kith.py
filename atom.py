aa5a2f7343e9116ce61c6de5d3075da3e93831bc
sneakerheadfarb <37280572+sneakerheadfarb@users.noreply.github.com>
2018-08-25 13:22:31 -0400

    Add files via upload

---
+++ atom.py
@@ -0,0 +1,45 @@
+import requests
+import feedparser
+import json
+
+url = "https://kith.com/"
+keyword = "test"
+size_input = "7"
+found = "false"
+
+while found == "false":
+	print("LOOKING FOR PRODUCT!")
+	d = feedparser.parse('https://kith.com/collections/footwear.atom')
+	count = 0
+	products = len(d['entries'])
+	while count < products:
+		title = d['entries'][count]['title']
+		if keyword in title:
+			prod_title = d['entries'][count]['title']  
+			prod_link = d['entries'][count]['link'] 
+			found = "true"
+			entry_num = count
+			count = 100
+		else:
+			count = count + 1
+print(prod_title)
+print(prod_link)
+count = 0
+r2 = requests.get(prod_link + ".json")
+variants = json.loads(r2.text)
+found_size = "false"
+for variant in variants:
+    while found_size == "false":
+        variant_id = str(variants[u'product'][u'variants'][count][u'id'])
+        size = str(variants[u'product'][u'variants'][count]['title']).replace("u", "")
+        size = size.replace("'", "")
+        if size.find(size_input) == -1:
+            count = count + 1
+            found_size = "false"
+        else:
+            checkout_link = url + "cart/" + variant_id + ":1"
+            found_size = "true"
+            count = 0
+print(checkout_link)
+
+
